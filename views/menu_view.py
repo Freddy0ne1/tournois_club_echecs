01e808b1c52187d718c6fe24766e1e378b5fd90d
Freddy0ne1 <freddykhuti@gmail.com>
2025-07-22 16:05:01 +0200

    Modification projet

--- views/menu_view.py
+++
@@ -1,8 +0,0 @@
-"""views/menu_view.py
-Affiche le menu principal du programme.
-"""
-
-
-def welcome():
-    """Affiche un message de bienvenue."""
-    print("=== Bienvenue dans le gestionnaire de tournois d'échecs ! ===")
